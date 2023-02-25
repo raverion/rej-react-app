e2fe7f19b48251d0800d3369d14ea8ccb1439c4d
Rej Averion <averionrej@gmail.com>
2023-02-25 14:18:58 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function App(props) {
+  const message = props.message;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{message}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
+  // const pElement = screen.getByText(/React To-do App/i);
   expect(linkElement).toBeInTheDocument();
+  // expect(pElement).toBeInTheDocument();
 });
